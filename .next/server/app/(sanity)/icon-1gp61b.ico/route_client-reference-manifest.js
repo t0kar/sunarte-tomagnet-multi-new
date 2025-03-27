6e3119a6713b51bbd1ea4279fbb06a2ad623dd56
Toma <81991067+t0kar@users.noreply.github.com>
2025-03-28 00:46:53 +0100

    Delete .next directory

--- .next/server/app/(sanity)/icon-1gp61b.ico/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/(sanity)/icon-1gp61b.ico/route"] = {"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
